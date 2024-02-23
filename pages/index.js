7d89639cbf1d2553d68c0374c11b51be6e56cb34
Luiza Moncao <luiza.cmoncao@gmail.com>
2024-02-23 01:18:18 -0300

    alteração de frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-  return <h1>É justo que muito custe aquilo que muito vale.</h1>
+  return(
+    <>
+      <h2>Estou amando o curso.dev! Muito obrigada!!!</h2>
+      <h1>Te amo ❤️</h1>
+      <h2>Já deu certo! 🙌</h2>
+      <h3><i>"É justo que muito custe aquilo que muito vale."</i></h3>
+    </>
+  )
 }
 
 export default Home;
